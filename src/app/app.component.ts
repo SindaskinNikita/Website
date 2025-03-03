8e15eceebc029e817f5a0180c3cad21f17ddc58b
Dima <baza.505.05@gmail.com>
2025-03-03 13:43:05 +0500

    Обновление административной панели и заголовка приложения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -10,3 +11,8 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'untitled2';
 }
+
+export class AdminComponent {
+  title = 'Панель администратора';
+}
+

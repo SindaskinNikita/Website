75ae860d48fabe9af89bcdf59a954946fe29de54
Dima <baza.505.05@gmail.com>
2025-03-04 09:45:12 +0500

    Обновление шаблона администратора и маршрутизации

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'admin', pathMatch: 'full' },
     { path: 'admin', component: AdminComponent }
 ];

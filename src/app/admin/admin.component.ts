99d5bb51ebf5be476aae88c6ad200c4b5108ad70
Dima <baza.505.05@gmail.com>
2025-03-04 10:40:37 +0500

    Обновление стилей и разметки административной панели

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
   title = 'Панель администратора';
+  imagePath = 'assets/your-image.png'; // укажите правильное имя файла
 }

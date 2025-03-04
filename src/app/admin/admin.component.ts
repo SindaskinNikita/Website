f83ff41b6a17441d2e21aea22ee5fdbb1cad3a89
Dima <baza.505.05@gmail.com>
2025-03-04 09:20:12 +0500

    Рефакторинг структуры проекта и маршрутизации

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
+  standalone: true,
   imports: [],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-
+  title = 'Панель администратора';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
-    { path: 'admin', component: AdminComponent },
+    { path: '', redirectTo: 'admin', pathMatch: 'full' },
+    { path: 'admin', component: AdminComponent }
 ];

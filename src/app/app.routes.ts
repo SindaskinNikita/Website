9c5f3907035a773825b9e1073988cdafbdca959b
Dima <baza.505.05@gmail.com>
2025-03-10 12:24:06 +0500

    Добавление маршрута для главного представления приложения

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { MainViewComponent } from '../main-view/main-view.component';
 
 export const routes: Routes = [
-    { path: 'admin', component: AdminComponent }
+    { path: 'admin', component: AdminComponent },
+    { path: '', component: MainViewComponent}
 ];
